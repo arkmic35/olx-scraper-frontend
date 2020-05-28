14a542bff14ebb973afe1b7e0879e9b39f78ae79
Arkadiusz Michalak <arkmic35@gmail.com>
2020-05-28 13:45:52 +0200

    Imports cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {OffersListComponent} from './component/offerslist/offers-list.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+
 import {KeywordFormComponent} from './component/keyword-form/keyword-form.component';
+import {OffersListComponent} from './component/offerslist/offers-list.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
--- src/app/component/offerslist/offers-list.component.ts
+++ src/app/component/offerslist/offers-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+
 import {OfferService} from "../../service/offer.service";
 import {ListStatus} from "./list.status";
 
